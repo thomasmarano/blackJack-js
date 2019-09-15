a74503f400593691820e8783bcac4c2d70a0727e
thomasmarano <thomas.marano2@gmail.com>
2019-09-15 15:56:45 +0100

    :white_check_mark: add first failing test

---
+++ spec/gameSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Game', function(){
+
+    var game = new Game();
+
+    describe('Initialization Tests', function(){
+        it('starts score on 0', function(){
+            expect(game.score).toEqual(0)
+        });
+    });
+});
