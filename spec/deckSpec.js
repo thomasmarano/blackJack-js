2f2afc278e95efc1633c66059c79913c689f3773
thomasmarano <thomas.marano2@gmail.com>
2019-09-15 17:12:12 +0100

    :white_check_mark: add test for dealing a card

--- spec/deckSpec.js
+++ spec/deckSpec.js
@@ -10,4 +10,13 @@ describe('Deck', function(){
             expect(deck.cards.length).toEqual(52);
         });
     });
+
+    describe('Basic Deck Functionality', function(){
+        it('deals the top card', function(){
+            var firstCard = deck.cards[0];
+            expect(deck.deal()).toEqual(firstCard);
+            expect(deck.cards.length).toEqual(51)
+        });
+    });
+
 });
