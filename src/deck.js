31ad0119ca5b99f33320d152054937009c4802ea
thomasmarano <thomas.marano2@gmail.com>
2019-09-15 17:12:37 +0100

    :sparkles: pass test of deck dealing first card

--- src/deck.js
+++ src/deck.js
@@ -6,12 +6,15 @@ function Deck() {
     var suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
     var values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
 
-    for (let index in suits) {
-        for (let index in values) {
-            this.cards.push([suits[index], values[index]])
+    for (let index1 in suits) {
+        for (let index2 in values) {
+            this.cards.push([suits[index1], values[index2]])
         }
     }
 }
 
+Deck.prototype.deal = function(){
+  return this.cards.shift();
+};
 
 module.exports = Deck;
