c930b3a3eb58f39ca3ac1c48dd9fbebdb48f6ed2
thomasmarano <thomas.marano2@gmail.com>
2019-09-16 15:26:52 +0100

    :white_check_mark: Add first feature test for starting new game

---
+++ spec/featureSpec.js
@@ -0,0 +1,24 @@
+var Hand = require('../src/hand');
+var Deck = require('../src/deck');
+
+describe('Feature Tests', function(){
+
+   var hand = new Hand();
+   var deck = new Deck();
+
+   beforeEach(function(){
+      deck.reset();
+      deck.shuffle();
+   });
+
+   describe('New Game Starts', function(){
+       it('hand receives two cards', function(){
+           hand.startGame();
+           expect(hand.currCards.length).toEqual(2);
+           expect(deck._suits.includes(hand.currCards[0][0])).toBeTruthy();
+           expect(deck._suits.includes(hand.currCards[1][0])).toBeTruthy();
+           expect(deck._values.includes(hand.currCards[0][1])).toBeTruthy();
+           expect(deck._values.includes(hand.currCards[1][1])).toBeTruthy();
+       });
+   });
+});
