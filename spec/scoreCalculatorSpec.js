15f659219a7679a0aa7e3ac6c20006ca3f1b1724
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 16:18:52 +0100

    :white_check_mark: Add test calculating exactly 2 & 2+ non-face cards

--- spec/scoreCalculatorSpec.js
+++ spec/scoreCalculatorSpec.js
@@ -8,7 +8,15 @@ describe('Score Calculator', function(){
        scorer = new ScoreCalculator();
    });
 
+    it('correctly calculates the value of 2 non-face cards', function(){
+        expect(scorer.calculate([[ 'Clubs', 9 ], [ 'Diamonds', 6 ]])).toEqual(15);
+        expect(scorer.calculate([[ 'Hearts', 4 ], [ 'Spades', 7 ]])).toEqual(11)
+    });
 
+    it('correctly calculates the value of 2+ non-face cards', function(){
+        expect(scorer.calculate([[ 'Clubs', 9 ], [ 'Spades', 6 ], [ 'Spades', 4 ], [ 'Spades', 7 ]])).toEqual(26);
+        expect(scorer.calculate([[ 'Clubs', 5 ], [ 'Diamonds', 8 ], [ 'Hearts', 3 ]])).toEqual(16);
 
+    })
 
 });
