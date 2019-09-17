788b4fe6e95aa1822f0fea14aa7ddf904cdf9c74
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 16:20:53 +0100

    :sparkles: Add passing code to calculate exactly 2 & 2+ non-face cards

--- src/scoreCalculator.js
+++ src/scoreCalculator.js
@@ -4,5 +4,11 @@ function ScoreCalculator(){
 
 }
 
+ScoreCalculator.prototype.calculate = function(cards){
+    return cards.reduce(function(sum, each){
+        return sum + each[1]
+        }, 0);
+};
+
 module.exports = ScoreCalculator;
 
