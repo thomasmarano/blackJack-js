9c7d6b0d232ed268fd6c60d4c1fe1056a0af26ca
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 13:12:01 +0100

    :clown_face: Insert mock for 'deck' into tests

--- src/hand.js
+++ src/hand.js
@@ -11,8 +11,6 @@ Hand.prototype.startGame = function(){
     for (var x = 0; x < 2; x++) {
         this.currCards.push(this.deck.deal());
     }
-    // console.log(this.currCards)
 };
-
 
 module.exports = Hand;
