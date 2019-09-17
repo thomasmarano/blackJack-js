3ff6a0f80451759e75c8d74f14ba26441c07efd0
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 12:50:19 +0100

    :white_check_mark: Add more specificity to 'start game' feature test

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -2,9 +2,8 @@ var Hand = require('../src/hand');
 var Deck = require('../src/deck');
 
 describe('Feature Tests', function(){
-
-   var hand = new Hand();
    var deck = new Deck();
+   var hand = new Hand(deck);
 
    beforeEach(function(){
       deck.reset();
@@ -15,6 +14,7 @@ describe('Feature Tests', function(){
        it('hand receives two cards from deck', function(){
            hand.startGame();
            expect(hand.currCards.length).toEqual(2);
+           expect(deck.cards.length).toEqual(50);
            expect(deck._suits.includes(hand.currCards[0][0])).toBeTruthy();
            expect(deck._suits.includes(hand.currCards[1][0])).toBeTruthy();
            expect(deck._values.includes(hand.currCards[0][1])).toBeTruthy();
