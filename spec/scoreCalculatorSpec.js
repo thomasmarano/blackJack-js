ca984b33a0785e99952bcfa083d60159109a2aa4
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 15:33:01 +0100

    :white_check_mark: & :sparkle: Add score calculator

---
+++ spec/scoreCalculatorSpec.js
@@ -0,0 +1,14 @@
+var ScoreCalculator = require('../src/scoreCalculator');
+
+describe('Score Calculator', function(){
+
+   var scorer;
+
+   beforeEach(function(){
+       scorer = new ScoreCalculator();
+   });
+
+
+
+
+});
---
+++ src/scoreCalculator.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function ScoreCalculator(){
+
+}
+
+module.exports = ScoreCalculator;
+
