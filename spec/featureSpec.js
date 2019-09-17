3cffd9d5ed9991e0c48ac90c241cc216e3689139
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 13:16:33 +0100

    :white_check_mark: add feature test so person receives card when chooses hit

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -10,8 +10,8 @@ describe('Feature Tests', function(){
       deck.shuffle();
    });
 
-   describe('New Game Starts', function(){
-       it('hand receives two cards from deck', function(){
+   describe('Game Logic', function(){
+       it('hand receives two cards from deck when game starts', function(){
            hand.startGame();
            expect(hand.currCards.length).toEqual(2);
            expect(deck.cards.length).toEqual(50);
@@ -19,6 +19,16 @@ describe('Feature Tests', function(){
            expect(deck._suits.includes(hand.currCards[1][0])).toBeTruthy();
            expect(deck._values.includes(hand.currCards[0][1])).toBeTruthy();
            expect(deck._values.includes(hand.currCards[1][1])).toBeTruthy();
+       });
+
+       it('deals 1 card from deck when person chooses hit', function(){
+          hand.startGame();
+          hand.hit();
+          expect(hand.currCards.length).toEqual(3);
+          expect(deck.cards.length).toEqual(49);
+          expect(deck._suits.includes(hand.currCards[2][0])).toBeTruthy();
+          expect(deck._values.includes(hand.currCards[2][1])).toBeTruthy();
+
        });
    });
 });
