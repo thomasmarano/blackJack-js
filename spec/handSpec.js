d64d94470f0dc0f6cdf8f7d7cca2b7ad6f3c906e
thomasmarano <thomas.marano2@gmail.com>
2019-09-17 13:33:09 +0100

    :white_check_mark: Add unit test for player choosing 'hit'

--- spec/handSpec.js
+++ spec/handSpec.js
@@ -1,10 +1,15 @@
 var Hand = require('../src/hand');
 
 describe('Hand', function(){
-    var deck = jasmine.createSpyObj('deckSpy', ['deal']);
-    var hand = new Hand(deck);
+    var deck;
+    var hand;
 
-    describe('Initialization Tests', function(){
+    beforeEach(function(){
+        deck = jasmine.createSpyObj('deckSpy', ['deal']);
+        hand = new Hand(deck);
+    });
+
+    describe('initialization tests', function(){
         it('starts score on 0', function(){
             expect(hand.score).toEqual(0)
         });
@@ -15,7 +20,7 @@ describe('Hand', function(){
 
     });
 
-    describe('Game Logic', function(){
+    describe('game logic', function(){
 
         beforeEach(function(){
 
@@ -30,9 +35,10 @@ describe('Hand', function(){
         });
 
         it('gives you a card when you hit', function(){
-
+            expect(hand.currCards.length).toEqual(0);
             hand.hit();
-
+            expect(hand.currCards.length).toEqual(1);
+            expect(deck.deal).toHaveBeenCalled()
         });
     });
 });
